5c6a97e693896586ea35d60bb34601d54935824b
pialcaja <pialcaja@gmail.com>
2026-02-07 12:37:22 -0500

    Construcción Front - Home-page

--- front/portfolio/src/app/projects/components/projects-card/project-card.ts
+++ front/portfolio/src/app/projects/components/projects-card/project-card.ts
@@ -15,15 +15,4 @@ export class ProjectsCard {
   @Input({ required: true })
   project!: Project;
 
-  onMouseMove(event: MouseEvent) {
-    const card = event.currentTarget as HTMLElement;
-    const rect = card.getBoundingClientRect();
-
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-
-    card.style.setProperty('--x', `${x}px`);
-    card.style.setProperty('--y', `${y}px`);
-  }
-
 }
